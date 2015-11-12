8ecc007840a065f13e2ea3bf36b82d09de82489e
nge chee keen <david.nnck@gmail.com>
2015-11-12 10:11:27 +0800

    updated solution for prob 3

--- prob-3.py
+++ prob-3.py
@@ -1,32 +1,41 @@
+import math
 #The prime factors of 13195 are 5, 7, 13 and 29.
 
 #What is the largest prime factor of the number 600851475143 ?
 
-def is_prime(n):
-    for i in range(2, n):
-        if n % i == 0:
+def isPrime(number):
+    if number == 2:
+       return true
+    if number % 2 == 0:
+        return False
+    
+    i = 3
+    sqrtOfNumber = math.sqrt(number)
+    
+    while i <= sqrtOfNumber:
+        if number % i == 0:
             return False
-    return True
+        i = i+2
+        
+    return True 
 
-def get_prime(divisible_list):
-	prime = []
-	for num in divisible_list:
-		if is_prime(num):
-			prime.append(num)
-	print prime[-1]
 
-def get_divisible(n):
+def factors(n):    
+    return set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+def compute(n):
 	
-	is_divisible = []
-	start = 2
-	for i in range(start,n):
-		if n%i==0:
-			is_divisible.append(i)
+	a = factors(n)
+	prime = []
+	for i in a:
+		if isPrime(i):
+			prime.append(i)
+	print prime
 
-	get_prime(is_divisible)
+		
 
-get_divisible(600851475143)
-			
+compute(600851475143)
 
 
 
